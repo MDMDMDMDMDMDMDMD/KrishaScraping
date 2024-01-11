8aee6abd7818497ab33b4c04ece134d2723c65e8
MD <dorianlaughor@gmail.com>
2024-01-12 02:46:33 +0600

    add comments

--- main.py
+++ main.py
@@ -4,29 +4,34 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
+# Load environment variables from the .env file
 load_dotenv()
 
+# Connect to MongoDB using the provided URI
 mongodb_uri = os.getenv("MONGODB_URI")
 cluster = MongoClient(mongodb_uri)
 db = cluster["testdb"]
 collection = db["advertisements"]
 
+# URL of the website to scrape
 url = "https://krisha.kz/arenda/kvartiry/almaty/?das[flat.floor][to]=500000"
+
+# Make a request to the website and get the HTML content
 req = requests.get(url)
 src = req.text
 
-#Если нужно сохранить страницу
-
-#Сохраняет html
+# Uncomment the following block if you want to save the HTML content to a file
 # with open("index.html", "w", encoding="utf-8") as file:
 #     file.write(src)
 
-#Считывает html
+# Uncomment the following block if you want to read the HTML content from a file
 # with open("index.html", encoding="utf-8") as file:
 #     src = file.read()
 
+# Create a BeautifulSoup object for parsing HTML
 soup = BeautifulSoup(src, "lxml")
 
+# Extract image sources, card titles, subtitles, text previews, and prices
 img_tags = soup.find_all("img", class_="a-image__img")
 img_src_list = [img_tag.get('src') for img_tag in img_tags]
 
@@ -35,8 +40,10 @@ disc_a_list = [elem.text.strip() for elem in soup.find_all("div", class_="a-card
 disc_b_list = [elem.text.strip() for elem in soup.find_all("div", class_="a-card__text-preview")]
 price_tags = [elem.text.strip() for elem in soup.find_all("div", class_="a-card__price")]
 
+# Determine the minimum length to avoid index out of range errors
 min_length = min(len(img_src_list), len(card_titles), len(disc_a_list), len(disc_b_list), len(price_tags))
 
+# Create a list of dictionaries containing the scraped data
 data_list = []
 for i in range(min_length):
     data = {
@@ -48,6 +55,8 @@ for i in range(min_length):
     }
     data_list.append(data)
 
+# Insert the scraped data into the MongoDB collection
 collection.insert_many(data_list)
 
+# Close the MongoDB connection
 cluster.close()
